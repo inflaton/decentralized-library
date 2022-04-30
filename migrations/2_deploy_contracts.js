d04080fefd9903aeec670989ed36789f7963c4cb
inflaton <inflaton.sg@gmail.com>
2022-04-30 23:45:26 +0800

    removed Users contract & UI code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
 var LibraryContract = artifacts.require("./Library.sol");
 var LibraryNftContract = artifacts.require("./LibraryNftContract.sol");
-var UsersContract = artifacts.require("./Users.sol");
 
 module.exports = function (deployer) {
    deployer.deploy(LibraryContract);
    deployer.deploy(LibraryNftContract);
-   deployer.deploy(UsersContract);
 };
